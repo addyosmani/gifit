bb15fb28ca1ecd53ed33d72237a6b21d2e69f812
Addy Osmani <addyosmani@gmail.com>
2013-06-28 11:20:27 +0100

    more hackery

--- js/cam.js
+++ js/cam.js
@@ -314,3 +314,19 @@ $(function () {
     window.webagram.activity = document.getElementById('activity');
 
 });
+
+
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight){
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
+
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
